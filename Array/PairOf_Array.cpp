f5b486160bddc230f2d0a4386d6e75b159a4ae7f
Hitesh Prajapati <hp5741609@gmail.com>
2023-02-10 18:14:20 +0530

    Array Questions

--- Array/PairOf_Array.cpp
+++ Array/PairOf_Array.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 void printAllPairs(int arr[],int n){
-
+        for(int i=0;i<n;i++){
+            int x = arr[i];
+            for(int j=i+1;j<n;j++){ // for all previous pair (j = 0 to j<n j++)
+                cout<<"("<<x<<","<<j<<")"<<endl;
+            }
+            cout<<endl;
+        }
 }
 
 int main(){
@@ -10,8 +16,6 @@ int main(){
     int arr[] = {1,2,3,4,5,6,7};
     int n = sizeof(arr)/sizeof(int);
 
-    for(int x : arr){
-        cout << x << endl;
-    }
+    printAllPairs(arr,n);
 
 }
