242c0be50f13b58bbb0494fe63fa96915cd6d794
Hitesh <hp5741609@gmail.com>
2023-10-02 10:00:27 +0530

    Print All Maze Path

---
+++ Recursion/mazepath_print.cpp
@@ -0,0 +1,26 @@
+// Print All Maze Path to reach at last element in 2D Matrix
+#include<bits/stdc++.h>
+using namespace std;
+
+void f(int i,int j,int m, int n, string output){
+
+    // base case
+    if(i==m-1 && j==n-1){
+        cout << output << endl;
+        return;
+    }
+    if(i>=m || j>=n){
+        return;
+    }
+
+    f(i+1,j,m,n,output+"R");
+    f(i,j+1,m,n,output+"D");
+}
+
+int main(){
+    int m,n;
+    cin>>m>>n;
+
+    f(0,0,m,n,"");
+    return 0;
+}
